7ddc5da3a327c9da17e384669af250a77688a284
Khottaewritecode <thamma4352@gmail.com>
2025-11-18 13:52:28 +0700

    1,2,5

---
+++ LabSheet01/src/Ex103.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Ex103 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "I am Phoptham Maisaphol\nI want grad A",
+                "Welcome message",
+                JOptionPane.WARNING_MESSAGE);
+    }
+}
