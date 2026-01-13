ffe7138ba161d61a3a2b8c9def37a6e57de17701
Khottaewritecode <thamma4352@gmail.com>
2026-01-13 13:38:10 +0700

    ภพธรรม ไม่ซาผล labsheet07

---
+++ Product.java
@@ -0,0 +1,35 @@
+public class Product {
+    public String name;
+    public double price=0;
+    public int quantity=0;
+
+    public void addStock(int amount){
+        if(amount>0){
+        quantity+=amount;
+    }
+    }
+    public void sell(int amount){
+        if(amount<=quantity){
+            quantity-=amount;
+            System.out.println("Sold ["+amount+"] item ");
+        }else{
+            System.out.println(" Not enough items in stock…");
+        }
+
+    }
+    public void changePrice(double newPrice){
+        if (newPrice>0)
+            price=newPrice;
+        System.out.println("Change price  to "+price+" Baht");
+        System.out.println();
+    }
+    public double stockvalue(){
+        return price*quantity;
+    }
+    public void showInfo(){
+        System.out.println("Name   : "+name);
+        System.out.println("Price  : "+price+" Baht/Item");
+        System.out.println("Stock  : "+quantity+" Items");
+        System.out.println("Stock Value : "+ stockvalue()+" Baht");
+    }
+}
